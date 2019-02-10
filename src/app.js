a6df493656000ba4e45ffd7d1d8bddce21df6fde
divyasrichindam <divyasrichindam02@gmail.com>
2019-02-10 12:54:37 -0600

    webpack config

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
-import './utils.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running');
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template,document.getElementById('app'));
+
+ 
